87d41ae334ed2550b045fd2c5c21bc8b420dda23
BobSnyder <bob@bobsnyderdesign.com>
2017-03-11 12:41:30 -0800

    initial

---
+++ Sources/App/main.swift
@@ -0,0 +1,28 @@
+import Vapor
+import VaporPostgreSQL
+
+
+let drop = Droplet(
+	providers: [VaporPostgreSQL.Provider.self]
+)
+
+drop.get("version") { reqest in
+	if let db = drop.database?.driver as? PostgreSQLDriver {
+		let version = try db.raw("SELECT version()")
+		return try JSON(node: version)
+	} else {
+		return "No db connection"
+	}
+//	return let
+	
+}
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
