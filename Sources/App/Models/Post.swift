c7f24fb034b5866ae13b7f14e4f5478d992c6199
BobSnyder <bob@bobsnyderdesign.com>
2017-03-11 16:17:23 -0800

    fight class

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -43,3 +43,57 @@ extension Post: Preparation {
         //
     }
 }
+
+final class Fight {
+	var id: Node?
+	var user1: Int
+	var user2: Int
+	var title: String
+	var one: Argument?
+	var two: Argument?
+	var three: Argument?
+	var four: Argument?
+	var five: Argument?
+	var six: Argument?
+	var seven: Argument?
+	var eight: Argument?
+	var nine: Argument?
+	var ten: Argument?
+	
+	init(user1: Int, user2: Int, title: String) {
+		self.user1 = user1
+		self.user2 = user2
+		self.title = title
+	}
+	//	init(node: Node, in context: Context) throws {
+	//		id = try node.extract("id")
+	//		namer = try node.extract("name")
+	//	}
+	
+	static func prepare(_ database: Database) throws {
+		try database.create("fights") { users in
+			users.id()
+			users.string("name")
+		}
+	}
+	static func returnList() -> [Fight] {
+		let fight1 = Fight(user1: 289, user2: 562, title: "BitchWHAAAT")
+		let fight2 = Fight(user1: 432, user2: 676, title: "Chaz Drinks Coke")
+		let fight3 = Fight(user1: 999, user2: 902, title: "Turn down for")
+		let fight4 = Fight(user1: 969, user2: 696, title: "Ethical implications of the treaty of 1862")
+		return [fight1, fight2, fight3, fight4]
+	}
+}
+
+
+
+final class Argument {
+	var theArgument: String
+	var link: String
+	
+	init(phrase: String, link: String) {
+		self.theArgument = phrase
+		self.link = link
+	}
+}
+
